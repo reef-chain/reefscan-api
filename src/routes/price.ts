032fc653db1da593bbda5dc33a5b8e3390e845c5
matjazonline <matjazonline@gmail.com>
2023-03-10 21:32:55 +0100

    cache price 30s

--- src/routes/price.ts
+++ src/routes/price.ts
@@ -1,19 +1,20 @@
 import {fetchReefPrice, Price} from "../services/utils";
 import express, { Response, Request, NextFunction } from 'express';
 
-let cachedPrice:{time: number, price?: Price}={time:0}
+const CACHE_PRICE_MS = 30000;
+let currentPrice:{time: number, price?: Price}={time:0}
 
 export const getReefPrice = async (_, res: Response, next: NextFunction) => {
     const now = (new Date()).getTime();
 
-    if (now - cachedPrice.time > 10000) {
+    if (now - currentPrice.time > CACHE_PRICE_MS) {
         try {
             const price = await fetchReefPrice();
-            cachedPrice = {time: (new Date()).getTime(), price};
+            currentPrice = {time: (new Date()).getTime(), price};
         } catch (err) {
             next(err);
         }
     }
-    res.send({...cachedPrice.price, time:cachedPrice.time});
+    res.send(currentPrice);
 
 }
