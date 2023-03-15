1d1d70a4f78a5e4568cfa00d95a1d6bb79c0fdda
matjazonline <matjazonline@gmail.com>
2023-03-15 17:09:47 +0100

    db connection with object param

--- src/db/sequelize.db.ts
+++ src/db/sequelize.db.ts
@@ -2,18 +2,18 @@ import { Sequelize } from "sequelize-typescript";
 import config from "../utils/config";
 import { VerifiedContractMainnet, VerifiedContractTestnet } from "./VerifiedContract.db";
 
-let connStr = `postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbName}`;
-export const sequelize = new Sequelize(connStr, {
-    models: [VerifiedContractMainnet, VerifiedContractTestnet],
-    repositoryMode: true,});
-// export const sequelize = new Sequelize(
-//   config.dbName,
-//   config.dbUser,
-//   config.dbPassword,
-//   {
-//     host: config.dbHost,
-//     dialect: "postgres",
-//     models: [VerifiedContractMainnet, VerifiedContractTestnet],
-//     repositoryMode: true,
-//   }
-// );
+//let connStr = `postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbName}`;
+//export const sequelize = new Sequelize(connStr, {
+//    models: [VerifiedContractMainnet, VerifiedContractTestnet],
+//    repositoryMode: true,});
+ export const sequelize = new Sequelize(
+   config.dbName,
+   config.dbUser,
+   config.dbPassword,
+   {
+     host: config.dbHost,
+     dialect: "postgres",
+     models: [VerifiedContractMainnet, VerifiedContractTestnet],
+     repositoryMode: true,
+   }
+ );
