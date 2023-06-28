de1c2c0c67c91faf68bacce95d2aa79904e80be2
boo-0x <boo_0x@tutanota.com>
2023-06-28 12:36:34 +0200

    Fix migration script

--- migrations/20230628062601-addIconUrl.js
+++ migrations/20230628062601-addIconUrl.js
@@ -3,20 +3,32 @@
 const { Sequelize } = require('sequelize');
 
 /** @type {import('sequelize-cli').Migration} */
+
+const tableMainnet = 'verified_contract_mainnet';
+const tableTestnet = 'verified_contract_testnet';
+const columnName = 'iconUrl'; 
+
 module.exports = {
   async up ({ context: queryInterface }) {
-    await queryInterface.addColumn('verified_contract_mainnet', 'iconUrl', { 
-      type: Sequelize.STRING,
-      allowNull: true,
-    });
-    await queryInterface.addColumn('verified_contract_testnet', 'iconUrl', { 
-      type: Sequelize.STRING,
-      allowNull: true,
-    });
+    const tableMainnetInfo = await queryInterface.describeTable(tableMainnet);
+    if (!tableMainnetInfo[columnName]) {
+      await queryInterface.addColumn(tableMainnet, columnName, { 
+        type: Sequelize.STRING,
+        allowNull: true,
+      });
+    }
+
+    const tableTestnetInfo = await queryInterface.describeTable(tableTestnet);
+    if (!tableTestnetInfo[columnName]) {
+      await queryInterface.addColumn(tableTestnet, columnName, { 
+        type: Sequelize.STRING,
+        allowNull: true,
+      });
+    }
   },
 
   async down ({ context: queryInterface }) {
-    await queryInterface.removeColumn('verified_contract_mainnet', 'iconUrl');
-    await queryInterface.removeColumn('verified_contract_testnet', 'iconUrl');
+    await queryInterface.removeColumn(tableMainnet, columnName);
+    await queryInterface.removeColumn(tableTestnet, columnName);
   }
 };
