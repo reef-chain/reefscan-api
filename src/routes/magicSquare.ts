8c5dcbc2142d136b17b1dff7ed948b7362b163c4
matjazonline <matjazonline@gmail.com>
2023-11-18 00:26:20 +0100

    remove logs

--- src/routes/magicSquare.ts
+++ src/routes/magicSquare.ts
@@ -1,7 +1,7 @@
 // TODO @anukulpandey: remove after campaign is over
-import { Router } from 'express';
-import { sequelize } from '../db/sequelize.db';
-import { MagicSquare } from '../db/MagicSquare.db';
+import {Router} from 'express';
+import {sequelize} from '../db/sequelize.db';
+import {MagicSquare} from '../db/MagicSquare.db';
 import axios from 'axios';
 
 
@@ -9,22 +9,22 @@ const router = Router();
 
 const baseUrl = "https://magic.lol/4bbad3f1"
 type Network = 'mainnet' | 'testnet';
+
 enum EventType {
     'swap' = 7,
 };
 
 interface MagicSquareParams {
-    msUserId:string;
-    eventType:EventType;
-    network:Network;
-    address:string;
+    msUserId: string;
+    eventType: EventType;
+    network: Network;
+    address: string;
 }
 
 router.post('/', async (req, res) => {
     try {
-        const { msUserId, eventType, network, address }:MagicSquareParams = req.body;
-console.log('body=', req.body)
-console.log('uid=', msUserId)
+        const {msUserId, eventType, network, address}: MagicSquareParams = req.body;
+
         let magicSquareRecord: MagicSquare | null = await sequelize.getRepository(MagicSquare).findOne({
             where: {
                 msUserId,
@@ -33,9 +33,8 @@ console.log('uid=', msUserId)
             }
         });
         if (magicSquareRecord) {
-            magicSquareRecord.eventCount +=1;
+            magicSquareRecord.eventCount += 1;
             await magicSquareRecord.save();
-            console.log('eventC=',magicSquareRecord.eventCount);
         } else {
             await sequelize.getRepository(MagicSquare).create({
                 msUserId,
@@ -46,21 +45,18 @@ console.log('uid=', msUserId)
             } as any);
         }
 
-        const ip = await axios.get(`https://api.ipify.org`);
-        console.log('ip=',ip);
-
         await axios.get(`${baseUrl}/pixel`, {
             params: {
-              vid: msUserId,
+                vid: msUserId,
                 action: EventType[eventType]
             }
-          });
+        });
         res.status(200).send({
             data: "updated data successfully"
         });
         return;
     } catch (error) {
-        console.error('ERR=',error);
+        console.error('ERR=', error?.request?.data ? error?.request?.data : error);
         return res.status(500).send({
             error: "Internal Server Error"
         });
