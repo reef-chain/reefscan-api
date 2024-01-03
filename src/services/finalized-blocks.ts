32cee22a8c07a1fcc3a68d18f9377a9d0bace11c
boo-0x <boo_0x@tutanota.com>
2024-01-03 12:23:00 +0100

    trackFinalizedBlocks: try-catch and logging

--- src/services/finalized-blocks.ts
+++ src/services/finalized-blocks.ts
@@ -2,18 +2,25 @@ import { getProvider, mutate } from "../utils/connector";
 
 export const trackFinalizedBlocks = async () => {
     let processing = false;
+
     getProvider().api.rpc.chain.subscribeFinalizedHeads(async (header) => {
         if (processing) return;
         processing = true;
+        console.log('trackFinalizedBlocks - new block:', header.number.toNumber());
 
-        await mutate(`
-            mutation {
-                newFinalizedBlock(
-                    height: ${header.number},
-                    hash: "${header.hash.toHuman()}"
-                )
-            }
-        `);
-        processing = false;
+        try {
+            await mutate(`
+                mutation {
+                    newFinalizedBlock(
+                        height: ${header.number},
+                        hash: "${header.hash.toHuman()}"
+                    )
+                }
+            `);
+            processing = false;
+        } catch (e) {
+            console.log('trackFinalizedBlocks - error:', e);
+            processing = false;
+        }
     });
 };
