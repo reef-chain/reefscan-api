76b46ef7125d2bece66ab662c550306724bf883f
matjazonline <matjazonline@gmail.com>
2024-07-22 13:55:48 +0200

    price error after set time

--- src/routes/price.ts
+++ src/routes/price.ts
@@ -4,21 +4,23 @@ import config from "../utils/config";
 
 
 const CACHE_PRICE_MS = 60000;
-let currentPrice:{timestamp: number, price?: Price}={timestamp:0}
+const CACHED_PRICE_ERROR_AFTER_MS = 600000;
+let currentPrice: { timestamp: number, price?: Price; } = {timestamp: 0};
 
 export const getReefPrice = async (_, res: Response, next: NextFunction) => {
     const now = (new Date()).getTime();
 
     if (now - currentPrice.timestamp > CACHE_PRICE_MS) {
         try {
+            currentPrice.timestamp = now;
             const price = await fetchReefPrice();
             currentPrice = {timestamp: (new Date()).getTime(), price};
         } catch (err) {
-            // TODO remove
-            currentPrice = {timestamp: (new Date()).getTime(), price:{usd:0.00145339, usd_24h_change:2.834696428592735, failed:true}};
-            console.log("REMOVE WHEN coingecko API ENABLED")
-            //next(err);
-            // return;
+            // if price didn't refresh for longer time return error
+            if(now - currentPrice.timestamp > CACHED_PRICE_ERROR_AFTER_MS) {
+                next(err);
+                return;
+            }
         }
     }
     res.send({...currentPrice.price, timestamp: currentPrice.timestamp});
