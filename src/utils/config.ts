a036a178cf5d241a8a7c4b328f946f9bd9b4c8f2
Anukul Pandey <anukul.030601@gmail.com>
2024-10-11 15:05:47 +0530

    feat: max retries

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -62,6 +62,7 @@ export default {
   localBackup: process.env.LOCAL_BACKUP === 'true',
   ipfsGatewayId: process.env.IPFS_PROJECT_ID!,
   ipfsGatewayKey: process.env.IPFS_PROJECT_SECRET!,
+  infuraProjectId: process.env.INFURA_PROJECT_ID!,
   solidityScanEndpoint:process.env.SOLIDITY_SCAN_URL!,
   solidityScanToken:process.env.SOLIDITY_SCAN_TOKEN!,
   stealthexEndpoint:process.env.STEALTHEX_URL!,
